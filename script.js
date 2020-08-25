7085d267437d5135a2cc682109c9bde677cfb8a8
Taco_Barril <codingtaco@gmail.com>
2020-08-25 14:06:03 -0500

    added template strings

--- script.js
+++ script.js
@@ -5,14 +5,15 @@ var body = document.getElementById("gradient");
 var randomBtn = document.getElementById("random");
 
 //Prints default value 
-css.textContent = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+// css.textContent = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+css.textContent = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
 
 //Generate random Gradient
 function randomColor() {
     var random1 = "#" + Math.floor(Math.random()*16777215).toString(16);
     var random2 = "#" + Math.floor(Math.random()*16777215).toString(16);
-    body.style.background = "linear-gradient(to right, " + random1 + ", " + random2 + ")";
-    css.textContent = "linear-gradient(to right, " + random1 + ", " + random2 + ")";
+    body.style.background = `linear-gradient(to right, ${random1}, ${random2})`;
+    css.textContent = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
     color1.value = random1
     color2.value = random2
 
@@ -20,8 +21,8 @@ function randomColor() {
 
 //Gradient function by input
 function setGradient() {
-    body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-    css.textContent = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+    body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+    css.textContent = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
 }
 
 //Event listeners 
